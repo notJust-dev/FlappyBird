c7e2148a6e945697c5f9dd93e40df36c17a704c4
Vadim Savin <savinvadim1312@gmail.com>
2024-02-23 15:38:58 +0000

    Setup the base assets of Flappy Bird

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useWindowDimensions } from 'react-native';
+import { Canvas, useImage, Image } from '@shopify/react-native-skia';
 
-export default function App() {
+const App = () => {
+  const { width, height } = useWindowDimensions();
+
+  const bg = useImage(require('./assets/sprites/background-day.png'));
+  const bird = useImage(require('./assets/sprites/yellowbird-upflap.png'));
+  const pipeBottom = useImage(require('./assets/sprites/pipe-green.png'));
+  const pipeTop = useImage(require('./assets/sprites/pipe-green-top.png'));
+  const base = useImage(require('./assets/sprites/base.png'));
+
+  const pipeOffset = 0;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <Canvas style={{ width, height }}>
+      {/* BG */}
+      <Image image={bg} width={width} height={height} fit={'cover'} />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      {/* Pipes */}
+      <Image
+        image={pipeTop}
+        y={pipeOffset - 320}
+        x={width / 2}
+        width={103}
+        height={640}
+      />
+      <Image
+        image={pipeBottom}
+        y={height - 320 + pipeOffset}
+        x={width / 2}
+        width={103}
+        height={640}
+      />
+
+      <Image
+        image={base}
+        width={width}
+        height={150}
+        y={height - 75}
+        x={0}
+        fit={'cover'}
+      />
+
+      {/* Bird */}
+      <Image image={bird} y={height / 2} x={width / 4} width={64} height={48} />
+    </Canvas>
+  );
+};
+export default App;
