c4c2d9e5c77e4b2a977a63394000f43a06811bf8
Vadim Savin <savinvadim1312@gmail.com>
2024-02-23 16:23:21 +0000

    Bird physics

--- App.js
+++ App.js
@@ -6,8 +6,16 @@ import {
   Easing,
   withSequence,
   withRepeat,
+  useFrameCallback,
 } from 'react-native-reanimated';
 import { useEffect } from 'react';
+import {
+  GestureHandlerRootView,
+  GestureDetector,
+  Gesture,
+} from 'react-native-gesture-handler';
+
+const GRAVITY = 500;
 
 const App = () => {
   const { width, height } = useWindowDimensions();
@@ -20,6 +28,17 @@ const App = () => {
 
   const x = useSharedValue(width);
 
+  const birdY = useSharedValue(0);
+  const birdYVelocity = useSharedValue(100);
+
+  useFrameCallback(({ timeSincePreviousFrame: dt }) => {
+    if (!dt) {
+      return;
+    }
+    birdY.value = birdY.value + (birdYVelocity.value * dt) / 1000;
+    birdYVelocity.value = birdYVelocity.value + (GRAVITY * dt) / 1000;
+  });
+
   useEffect(() => {
     x.value = withRepeat(
       withSequence(
@@ -30,42 +49,50 @@ const App = () => {
     );
   }, []);
 
+  const gesture = Gesture.Tap().onStart(() => {
+    birdYVelocity.value = -300;
+  });
+
   const pipeOffset = 0;
 
   return (
-    <Canvas style={{ width, height }}>
-      {/* BG */}
-      <Image image={bg} width={width} height={height} fit={'cover'} />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <GestureDetector gesture={gesture}>
+        <Canvas style={{ width, height }}>
+          {/* BG */}
+          <Image image={bg} width={width} height={height} fit={'cover'} />
 
-      {/* Pipes */}
-      <Image
-        image={pipeTop}
-        y={pipeOffset - 320}
-        x={x}
-        width={103}
-        height={640}
-      />
-      <Image
-        image={pipeBottom}
-        y={height - 320 + pipeOffset}
-        x={x}
-        width={103}
-        height={640}
-      />
+          {/* Pipes */}
+          <Image
+            image={pipeTop}
+            y={pipeOffset - 320}
+            x={x}
+            width={103}
+            height={640}
+          />
+          <Image
+            image={pipeBottom}
+            y={height - 320 + pipeOffset}
+            x={x}
+            width={103}
+            height={640}
+          />
 
-      {/* Base */}
-      <Image
-        image={base}
-        width={width}
-        height={150}
-        y={height - 75}
-        x={0}
-        fit={'cover'}
-      />
+          {/* Base */}
+          <Image
+            image={base}
+            width={width}
+            height={150}
+            y={height - 75}
+            x={0}
+            fit={'cover'}
+          />
 
-      {/* Bird */}
-      <Image image={bird} y={height / 2} x={width / 4} width={64} height={48} />
-    </Canvas>
+          {/* Bird */}
+          <Image image={bird} y={birdY} x={width / 4} width={64} height={48} />
+        </Canvas>
+      </GestureDetector>
+    </GestureHandlerRootView>
   );
 };
 export default App;
